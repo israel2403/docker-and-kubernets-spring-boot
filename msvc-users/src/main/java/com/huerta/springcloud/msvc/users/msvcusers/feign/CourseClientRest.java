016a45627a9feaf15af050c826fcd94ae8e0cd76
Israel Huerta <israelhf24@gmail.com>
2023-05-13 12:20:38 -0400

    diferents ways to create an image

--- msvc-users/src/main/java/com/huerta/springcloud/msvc/users/msvcusers/feign/CourseClientRest.java
+++ msvc-users/src/main/java/com/huerta/springcloud/msvc/users/msvcusers/feign/CourseClientRest.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-courses", url = "msvc-courses:8002/courses")
+// @FeignClient(name = "msvc-courses", url = "msvc-courses:8002/courses")
+@FeignClient(name = "msvc-courses", url = "${msvc.courses.url}")
 public interface CourseClientRest {
   @DeleteMapping("users/{id}")
   public void deleteByUserId(@PathVariable(value = "id") final String id);
